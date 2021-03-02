0fa71f72a36be24b40c2fe769fc9287a0c00f23f
Tanner-Jones <56734340+Tanner-Jones@users.noreply.github.com>
2021-03-01 21:37:58 -0700

    Add files via upload

--- Client.py
+++ Client.py
@@ -12,8 +12,9 @@ sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT_SEND))
 
-
+sock2 = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
 print(HOSTNAME, UDP_PORT_RECEIVE)
-sock.bind((HOSTNAME, UDP_PORT_RECEIVE))
+sock2.bind((HOSTNAME, UDP_PORT_RECEIVE))
 data, addr = sock.recvfrom(1024)
 print(data)
