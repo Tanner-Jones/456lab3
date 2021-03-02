0708df0c7f791be81926714679656ec7cb82f3a0
Tanner-Jones <56734340+Tanner-Jones@users.noreply.github.com>
2021-03-01 22:50:15 -0700

    Add files via upload

--- Server.py
+++ Server.py
@@ -14,7 +14,7 @@ message_list = []
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print("\nRecent Messages:")
-    message_list.insert(0, data)
+    message_list.insert(0, (data, time.time()))
     sock2 = socket.socket(socket.AF_INET,  # Internet
                          socket.SOCK_DGRAM)  # UDP
     time.sleep(2)
@@ -22,7 +22,7 @@ while True:
     if len(message_list) > 5:
         message_list.pop(5)
     for message in message_list:
-        message_send = str(message_list.index(message) + 1) + ": " + message + ", IP: " + addr[0] + " Time: " + \
-                  str(time.ctime(time.time()))
+        message_send = str(message_list.index(message) + 1) + ": " + message[0] + ", IP: " + addr[0] + " Time: " + \
+                  str(time.ctime(message[1]))
         print(message_send)
         sock2.sendto(bytes(message_send), (addr[0], UDP_PORT_SEND))
