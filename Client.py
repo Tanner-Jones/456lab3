4a80b6b86f44eb6e9cec4d8062e0d319e274d88f
Tanner-Jones <56734340+Tanner-Jones@users.noreply.github.com>
2021-03-01 22:58:02 -0700

    Add files via upload

--- Client.py
+++ Client.py
@@ -6,17 +6,23 @@ UDP_IP = sys.argv[1]
 HOSTNAME = socket.gethostname()
 UDP_PORT_SEND = 5005
 UDP_PORT_RECEIVE = 4444
-MESSAGE = bytes(sys.argv[2])
 
-
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT_SEND))
-
-time.sleep(1)
-sock2 = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock2.bind((HOSTNAME, UDP_PORT_RECEIVE))
 while True:
-    data, addr = sock2.recvfrom(1024)
-    print(data)
+    mes = input("Enter Message:")
+    MESSAGE = bytes(mes)
+
+
+    sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT_SEND))
+
+    time.sleep(1)
+    sock2 = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+    sock2.bind((HOSTNAME, UDP_PORT_RECEIVE))
+    current_time = time.time()
+    while True:
+        data, addr = sock2.recvfrom(1024)
+        print(data)
+        if time.time() > (current_time + 10):
+            break
