5dfef14d9bab42237d62bf64f303a72826ee1bc5
Tanner-Jones <56734340+Tanner-Jones@users.noreply.github.com>
2021-03-01 21:22:02 -0700

    Add files via upload

--- Client.py
+++ Client.py
@@ -2,10 +2,15 @@ import socket
 import sys
 
 UDP_IP = sys.argv[1]
-UDP_PORT = 5005
+UDP_PORT_SEND = 5005
+UDP_PORT_RECEIVE = 5006
 MESSAGE = bytes(sys.argv[2])
 
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT_SEND))
+
+sock.bind(socket.gethostname(), UDP_PORT_RECEIVE)
+data, addr = sock.recvfrom(1024)
+print(data)
--- Server.py
+++ Server.py
@@ -1,7 +1,9 @@
 import socket
+import time
 
 UDP_IP = socket.gethostname()
-UDP_PORT = 5005
+UDP_PORT_RECEIVE = 5005
+UDP_PORT_SEND = 5006
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
@@ -10,8 +12,9 @@ message_list = []
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    message_list.insert(data, 0)
+    message_list.insert(0, data)
     if len(message_list) > 5:
         message_list.pop(5)
     for message in message_list:
-        sock.sendto(message, addr)
+        print(message.index(), message, time.time())
+        sock.sendto(message, (addr[0], UDP_PORT_SEND))
